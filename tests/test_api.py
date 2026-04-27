fcf8e8d8a9566a5843642005e26ae14108c62d84
Hayanson <ccammi1784@gmail.com>
2026-04-27 11:12:43 +0900

    chore : 테스트 케이스 추가

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,3 +13,22 @@ def test_process_non_image_file(client):
     }
     response = client.post('/process', data=data, content_type='multipart/form-data')
     assert response.status_code == 400
+
+def test_process_no_image_part(client):
+    """폼 데이터에 'image' 키 자체가 누락된 채로 전송되었을 때 400 에러 확인"""
+    # image 파일 없이 filter_type만 보냄
+    data = {'filter_type': 'grayscale'}
+    response = client.post('/process', data=data, content_type='multipart/form-data')
+    
+    assert response.status_code == 400
+
+def test_process_empty_filename(client):
+    """파일을 선택하지 않고 전송 버튼을 눌렀을 때 (빈 파일명) 400 에러 확인"""
+    data = {
+        # 파일 내용은 비어있고, 파일명도 '' 인 상태
+        'image': (io.BytesIO(b""), ''), 
+        'filter_type': 'grayscale'
+    }
+    response = client.post('/process', data=data, content_type='multipart/form-data')
+    
+    assert response.status_code == 400
